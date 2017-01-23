544722cb404063a97d044f829e8b46ed98d46b10
usainzg <unaihtc70@gmail.com>
2017-01-23 10:15:26 +0100

    Group class added.

--- src/model/Group.java
+++ src/model/Group.java
@@ -1,5 +1,13 @@
 package model;
 
 public class Group {
+	
+	private String name;
+	private boolean isDirective;
+	
+	public Group(String name, boolean isDirective){
+		this.name = name;
+		this.isDirective = isDirective;
+	}
 
 }
