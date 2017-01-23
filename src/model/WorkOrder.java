3f676708c251bece836357e6983dcec70ce09d65
usainzg <unaihtc70@gmail.com>
2017-01-23 10:19:27 +0100

    WorkOrder class created.

--- src/model/WorkOrder.java
+++ src/model/WorkOrder.java
@@ -1,5 +1,23 @@
 package model;
 
+import java.util.Date;
+
 public class WorkOrder {
+	
+	private String workOrderId;
+	private int requiresTool;
+	
+	// FIXME
+	private String instructionsUrl;
+	private boolean isValidated;
+	private Date date;
+	
+	public WorkOrder(String workOrderId, int requiresTool, String instructionsUrl, boolean isValidated, Date date){
+		this.workOrderId = workOrderId;
+		this.requiresTool = requiresTool;
+		this.instructionsUrl = instructionsUrl;
+		this.isValidated = isValidated;
+		this.date = date;
+	}
 
 }
