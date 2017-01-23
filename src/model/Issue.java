b337e8dae96b3f717edbf33116507e6c5005e40a
usainzg <unaihtc70@gmail.com>
2017-01-23 10:22:05 +0100

    Issue class created.

--- src/model/Issue.java
+++ src/model/Issue.java
@@ -1,5 +1,21 @@
 package model;
 
+import java.util.Date;
+
 public class Issue {
+	
+	private String failureType;
+	private Date date;
+	private String subject;
+	private String description;
+	private String severity;
+	
+	public Issue(String failureType, Date date, String subject, String description, String severity){
+		this.failureType = failureType;
+		this.date = date;
+		this.subject = subject;
+		this.description = description;
+		this.severity = severity;
+	}
 
 }
