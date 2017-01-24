7d604969eae5732e183871bc2f4b7db920dee587
Daniel-Esteban <dastebi@hotmail.com>
2017-01-24 22:13:05 +0100

    Class Group modificated

--- src/model/Group.java
+++ src/model/Group.java
@@ -1,14 +1,27 @@
 package model;
 
+import java.util.Arrays;
+
 public class Group {
 	
-	private String name;
+	private String name;//TODO yo pondria esto en int --Dani
 	private boolean isDirective;
 	private User[] users;
 	
 	public Group(String name, boolean isDirective){
 		this.name = name;
 		this.isDirective = isDirective;
+	}
+	
+	public int getId(){
+		String id = name.substring(2, name.length());
+		return Integer.parseInt(id);
+	}
+	
+	public char getRole(){
+		char role = name.charAt(0);
+		role = Character.toUpperCase(role);
+		return role;
 	}
 
 	public String getName() {
@@ -21,6 +34,16 @@ public class Group {
 	
 	public User[] getUsers() {
 		return users;
+	}
+
+	
+	public void setUsers(User[] users) {
+		this.users = users;
+	}
+
+	@Override
+	public String toString() {
+		return "Group [\nname = " + name + ", \nisDirective = " + isDirective + ", \nusers = " + Arrays.toString(users) + "\n]";
 	}
 	
 }
