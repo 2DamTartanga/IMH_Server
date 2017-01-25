479ef2e90da036263b4140f9c3d2cf67141ce84d
Daniel Esteban <dastebi@hotmail.com>
2017-01-25 09:14:05 +0100

    New classes modified

---
+++ src/new_calsses/FailureLocalization.java
@@ -0,0 +1,20 @@
+package new_calsses;
+
+public class FailureLocalization {
+
+	private int id;
+	private String name;
+	public FailureLocalization(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	
+	
+}
--- src/new_calsses/Machine.java
+++ src/new_calsses/Machine.java
@@ -2,17 +2,15 @@ package new_calsses;
 
 public class Machine {
 
-	//TODO esto es seguramente va a cambiar
-	private int code; // String code;??
-	private int type; //??
+	private int code; 
+	private MachineType type;
 	private String serialNumber;
 	private int status;
 	private String year;
-	private int localization; //??
+	private Localization localization; //??
 	private int importance;
 	
 	private String phoneNumber;
-	private String model;
 	private String manager;
 	private String electricConnection;
 	private boolean pneumaticConnection;
@@ -24,6 +22,116 @@ public class Machine {
 	private String workingPressure;
 	private String manufacturer;
 	private String fileSearch;
+	public Machine(int code, MachineType type, int status, Localization localization, int importance) {
+		super();
+		this.code = code;
+		this.type = type;
+		this.status = status;
+		this.localization = localization;
+		this.importance = importance;
+	}
+	public int getCode() {
+		return code;
+	}
+	public MachineType getType() {
+		return type;
+	}
+	public String getSerialNumber() {
+		return serialNumber;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public String getYear() {
+		return year;
+	}
+	public Localization getLocalization() {
+		return localization;
+	}
+	public int getImportance() {
+		return importance;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public String getManager() {
+		return manager;
+	}
+	public String getElectricConnection() {
+		return electricConnection;
+	}
+	public boolean isPneumaticConnection() {
+		return pneumaticConnection;
+	}
+	public boolean isHydraulicConnection() {
+		return hydraulicConnection;
+	}
+	public String getDimmensions() {
+		return dimmensions;
+	}
+	public String getPowerSource() {
+		return powerSource;
+	}
+	public String getWeight() {
+		return weight;
+	}
+	public String getTypeOfOil() {
+		return typeOfOil;
+	}
+	public String getWorkingPressure() {
+		return workingPressure;
+	}
+	public String getManufacturer() {
+		return manufacturer;
+	}
+	public String getFileSearch() {
+		return fileSearch;
+	}
+	public void setSerialNumber(String serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public void setManager(String manager) {
+		this.manager = manager;
+	}
+	public void setElectricConnection(String electricConnection) {
+		this.electricConnection = electricConnection;
+	}
+	public void setPneumaticConnection(boolean pneumaticConnection) {
+		this.pneumaticConnection = pneumaticConnection;
+	}
+	public void setHydraulicConnection(boolean hydraulicConnection) {
+		this.hydraulicConnection = hydraulicConnection;
+	}
+	public void setDimmensions(String dimmensions) {
+		this.dimmensions = dimmensions;
+	}
+	public void setPowerSource(String powerSource) {
+		this.powerSource = powerSource;
+	}
+	public void setWeight(String weight) {
+		this.weight = weight;
+	}
+	public void setTypeOfOil(String typeOfOil) {
+		this.typeOfOil = typeOfOil;
+	}
+	public void setWorkingPressure(String workingPressure) {
+		this.workingPressure = workingPressure;
+	}
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+	public void setFileSearch(String fileSearch) {
+		this.fileSearch = fileSearch;
+	}
+	
+	
+	
 	
 	
 }
