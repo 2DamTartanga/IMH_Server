49e9e9bbbcdc24728ecf525497bd2213a66ee7ea
Unai Sainz De La Maza Gamboa <unaihtc70@gmail.com>
2017-02-01 11:50:33 +0100

    addRepair added.

--- src/db/DBRepair.java
+++ src/db/DBRepair.java
@@ -1,5 +1,6 @@
 package db;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import model.Group;
@@ -12,8 +13,29 @@ public class DBRepair extends NewDBManager {
 		
 	}
 	
-	public WorkOrder addRepair(WorkOrder workOrder){
+	public boolean addRepair(WorkOrder workOrder) throws SQLException{
+		boolean result = false;
 		
+		this.connect();
+		Repair r = workOrder.getRepairs().get(workOrder.getRepairs().size() - 1);
+		sql = "INSERT INTO repairs (codBreakdonw, idGroup, "
+				+ "repairDate, time, availabilityAfter, tools, repairProcess, "
+				+ "idLocalization, isRepaired, replacements) "
+				+ "VALUES ('" + workOrder.getBreakdown().getId() + "', '"
+				+ r.getGroup().getId() + "', '"
+				+ r.getDate() + "', '"
+				+ r.getTime() + "', '"
+				+ r.getRepairProcess() + "', '"
+				+ r.getFailureLocalization() + "', '"
+				+ r.isRepaired() + "', '"
+				+ r.getReplacements() + "', '"
+				+ "')";
+				
+		result = stmt.execute(sql);
+		
+		this.close();
+		
+		return result;
 	}
 	
 	public ArrayList<Repair> getRepairsFromGroup(Group group){
