5c068c7daf1efc833203f2a1cba085588270438c
TARTANGALH\2dam <ismaeltruebam@gmail.com>
2017-02-15 12:27:50 +0100

    Corregir singleton + loadProperties.

--- src/log/Logger.java
+++ src/log/Logger.java
@@ -4,13 +4,17 @@ import org.apache.log4j.PropertyConfigurator;
 
 public class Logger {
 
-	final static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(Logger.class);
+	private static org.apache.log4j.Logger log;
 	
 	public static org.apache.log4j.Logger xDD(){
+		if(log == null){
+			log = org.apache.log4j.Logger.getLogger(Logger.class);
+			loadProperty();
+		}
 		return log;
 	}
 	
 	public static void loadProperty(){
-		PropertyConfigurator.configure(Logger.class.getClassLoader().getResource("log4j.properties"));
+		PropertyConfigurator.configure(Logger.class.getClassLoader().getResource("log/log4j.properties"));
 	}
 }
