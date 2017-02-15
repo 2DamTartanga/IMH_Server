32a04aced0e22170e6df69fbdb42146151ec0964
TARTANGALH\2dam <ismaeltruebam@gmail.com>
2017-02-15 11:53:50 +0100

    Añadidos loggers ThreadClient.

--- src/threads/ThreadClient.java
+++ src/threads/ThreadClient.java
@@ -8,6 +8,7 @@ import java.net.Socket;
 import com.tartanga.dam.imhandroid.model.Message;
 
 import control.Manager;
+import log.Logger;
 
 
 public class ThreadClient extends Thread {
@@ -30,20 +31,26 @@ public class ThreadClient extends Thread {
 		try{
 			in = new ObjectInputStream(clientSocket.getInputStream());
 			input = in.readObject();
-			Logger.
+			Logger.xDD().info("ThreadClient -> Socket cliente ha enviado un objeto.");
 			if(input instanceof Message){
+				Logger.xDD().info("ThreadClient -> Socket cliente ha enviado un objeto del tipo Message.");
 				output = manager.manageMessages((Message) input);
+				Logger.xDD().info("ThreadClient -> Optenida salida de manageMessages().");
 				out.writeObject(output);
+				Logger.xDD().info("ThreadClient -> Enviada salida de manageMessages() a socket cliente.");
 			}
 		}catch(ClassNotFoundException e){
+			Logger.xDD().error("ThreadClient -> Error ClassNotFoundException.");
 			e.printStackTrace();
 		}catch(IOException e){
+			Logger.xDD().error("ThreadClient -> Error IOException.");
 			e.printStackTrace();
 		}finally{
 			try {
 				if(in != null) in.close();
 				if(out != null) out.close();
 				if(clientSocket != null) clientSocket.close();
+				Logger.xDD().info("ThreadClient -> Cerrados el socket y los streams.");
 			}catch(IOException ex) {
 				ex.printStackTrace();
 			}
