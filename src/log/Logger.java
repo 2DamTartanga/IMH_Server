f14a52131d949dbc0c9404ffad1e4eb21728af74
Daniel Esteban <dastebi@hotmail.com>
2017-02-15 10:57:50 +0100

    jejejej

--- src/log/Logger.java
+++ src/log/Logger.java
@@ -5,7 +5,11 @@ import org.apache.log4j.PropertyConfigurator;
 public class Logger {
 
 	final static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(Logger.class);
-
+	
+	public static org.apache.log4j.Logger xDD(){
+		return log;
+	}
+	
 	public static void loadProperty(){
 		PropertyConfigurator.configure(Logger.class.getClassLoader().getResource("log4j.properties"));
 	}
