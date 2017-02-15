cfde55d722f2e7624b01609f315e50b8794b9902
TARTANGALH\2dam <ismaeltruebam@gmail.com>
2017-02-15 11:59:34 +0100

    Limpieza

--- src/interfaces/Database.java
+++ src/interfaces/Database.java
@@ -19,18 +19,13 @@ public interface Database {
 	public Group getGroup(Group group) throws Exception;
 	
 	public boolean addBreakdown(Breakdown breakdown) throws Exception;
+	
 	public Breakdown getBreakdown(Breakdown breakdown) throws Exception;
 
 	public Machine getMachine(Machine machine) throws Exception;
 
-	/**
-	 * A�ade reparaci�n al WorkOrder
-	 * si el WorkOrder ya ten�a una reparaci�n resuelta, no hace nada
-	 * si el WorkOrder ya ten�a una reparaci�n no resuelta, la sustituye
-	 * @param workOrder
-	 * @return si la reparaci�n se ha a�adido correctamente
-	 */
 	public boolean addRepair(WorkOrder workOrder) throws Exception;
+	
 	public WorkOrder getRepair(WorkOrder workOrder) throws Exception;
 	
 	public User getUser(User user) throws Exception;
